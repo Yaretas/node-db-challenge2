1b0282983884cf1a3363ba7858f0b32d23b26d3e
Yarriba Castro <yarribacastro@Yarribas-MacBook-Pro.local>
2020-04-25 10:56:34 -0500

    Schema changes working

--- data/migrations/20200421205132_create-resources-table.js
+++ data/migrations/20200421205132_create-resources-table.js
@@ -1,14 +1,12 @@
 exports.up = function (knex) {
-    return knex.schema.createTable('Projects', tbl => {
-        tbl.increments('id');
-        tbl.text('name', 128)
-            .unique()
-            .notNullable();
-        tbl.text('description');
-        tbl.boolean('Completed');
-    });
+  return knex.schema.createTable("Projects", (tbl) => {
+    tbl.increments("id");
+    tbl.text("name", 128).unique().notNullable();
+    tbl.text("description");
+    tbl.boolean("Task Completed").default(false);
+  });
 };
 
 exports.down = function (knex) {
-    return knex.schema.dropTableIfExists('Projects');
+  return knex.schema.dropTableIfExists("Projects");
 };

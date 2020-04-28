72a5b0f3aa1c9eeeec73404f108aff4be9a73d70
Yarriba Castro <yarribacastro@Yarribas-MacBook-Pro.local>
2020-04-27 22:06:26 -0500

    Schema and seeds runing

--- data/migrations/20200427212250_task.js
+++ data/migrations/20200427212250_task.js
@@ -1,12 +1,11 @@
 exports.up = function (knex) {
-    return knex.schema.createTable("projects", (tbl) => {
+    return knex.schema.createTable("task", (tbl) => {
         tbl.increments("id");
         tbl.text("name", 128).unique().notNullable();
         tbl.text("description", 128);
-        tbl.boolean("Task Completed").default(false).notNullable();
     });
 };
 
 exports.down = function (knex) {
-    return knex.schema.dropTableIfExists("projects");
+    return knex.schema.dropTableIfExists("task");
 };
